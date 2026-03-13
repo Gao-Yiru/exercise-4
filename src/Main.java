b67acb63c9fb5a89bbd3784208bfe31aa3ce0d01
”Amber” <18256616490@163.com>
2026-03-13 23:06:29 +0800

    Feedback

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,17 @@ public  class Main {
         System.out.println("Correct usage of private method calculateFare(): Called internally via public method printBikeDetails.");
         System.out.println("Total fare for bike1 (10 minutes ride):" + bike1.getTotalFare());
         System.out.println("Total fare for bike2 (25 minutes ride):" + bike2.getTotalFare() + "USD");
+
+ 
+        String sent1 = "I was very satisfied with the service.";
+        String sent2 = "The e-Bike is quite comfortable to ride.";
+        String sent3 = "The battery life of the e-Bike is impressive.";
+        String sent4 = "The customer support was helpful and responsive.";
+        String sent5 = "I would recommend this e-Bike to my friends and family.";
+
+        Feedback feedBack = new Feedback("Alice", "Beauty", "4923das@163.com");
+        feedBack.analyseFeedBack(false, sent1, sent2, sent3, sent4, sent5); 
+        System.out.println(feedBack);
     }
     
 }
