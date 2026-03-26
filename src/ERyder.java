18d3b741e0a29f55edabd4f25e496a2b8a6e1a45
”Amber” <18256616490@163.com>
2026-03-26 23:40:40 +0800

    3.26

--- src/ERyder.java
+++ src/ERyder.java
@@ -1,75 +1,65 @@
 public class ERyder {
-    public static final String COMPANY_NAME = "ERyder";
-    public static final double BASE_FARE = 1.0;
-    public static final double PER_MINUTE_FARE = 0.5;
+   public static final String COMPANY_NAME = "ERyder";
+   public static final double BASE_FARE = (double)1.0F;
+   public static final double PER_MINUTE_FARE = (double)0.5F;
+   private final String LINKED_ACCOUNT;
+   private final String LINKED_PHONE_NUMBER;
+   private int bikeID;
+   private int batteryLevel;
+   private boolean isAvailable;
+   private double kmDriven;
+   private int totalUsageInMinutes;
+   private double totalFare;
 
-    private final String LINKED_ACCOUNT;
-    private final String LINKED_PHONE_NUMBER;
+   public ERyder(int var1, int var2, boolean var3, double var4) {
+      this.bikeID = var1;
+      this.batteryLevel = var2;
+      this.isAvailable = var3;
+      this.kmDriven = var4;
+      this.LINKED_ACCOUNT = "default_user";
+      this.LINKED_PHONE_NUMBER = "default_123456789";
+   }
 
+   public ERyder(int var1, int var2, boolean var3, double var4, String var6, String var7) {
+      this.bikeID = var1;
+      this.batteryLevel = var2;
+      this.isAvailable = var3;
+      this.kmDriven = var4;
+      this.LINKED_ACCOUNT = var6;
+      this.LINKED_PHONE_NUMBER = var7;
+   }
 
-    private int bikeID;
-    private int batteryLevel;
-    private boolean isAvailable;
-    private double kmDriven;
-    private int totalUsageInMinutes;
-    private double totalFare;
+   public void printRideDetails(int var1) {
+      this.totalUsageInMinutes = var1;
+      System.out.println("======= Ride Details =======");
+      System.out.println("The linked account is " + this.LINKED_ACCOUNT + ".");
+      System.out.println("The linked phone number is " + this.LINKED_PHONE_NUMBER + ".");
+      System.out.println("The bike ID is " + this.bikeID + ".");
+      System.out.println("The usage in minutes is " + this.totalUsageInMinutes + ".");
+      System.out.println("The total fare is " + calculateFare(var1) + ".");
+      System.out.println("============================\n");
+   }
 
-   
-    
-    public ERyder(int bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
-        this.bikeID = bikeID;
-        this.batteryLevel = batteryLevel;
-        this.isAvailable = isAvailable;
-        this.kmDriven = kmDriven;
-        this.LINKED_ACCOUNT = "default_user";
-        this.LINKED_PHONE_NUMBER = "default_123456789";
-    }
+   public void printBikeDetails() {
+      System.out.println("======= Bike Details =======");
+      System.out.println("The bike's ID is " + this.bikeID + ".");
+      System.out.println("The battery level is " + this.batteryLevel + "%.");
+      if (this.isAvailable) {
+         System.out.println("The bike is available.");
+      } else {
+         System.out.println("The bike is not available.");
+      }
 
-    public ERyder(int bikeID, int batteryLevel, boolean isAvailable, double kmDriven, String linkedAccount, String linkedPhoneNumber) {
-        this.bikeID = bikeID;
-        this.batteryLevel = batteryLevel;
-        this.isAvailable = isAvailable;
-        this.kmDriven = kmDriven;
-        this.LINKED_ACCOUNT = linkedAccount;
-        this.LINKED_PHONE_NUMBER = linkedPhoneNumber;
-    }
+      System.out.println("The bike's distance travelled in " + this.kmDriven + " km.");
+      System.out.println("============================\n");
+   }
 
-    public void printRideDetails(int usageInMinutes){
-        totalUsageInMinutes = usageInMinutes;
-        System.out.println("======= Ride Details =======");
-        System.out.println("The linked account is "+LINKED_ACCOUNT+".");
-        System.out.println("The linked phone number is "+LINKED_PHONE_NUMBER+".");
-        System.out.println("The bike ID is "+bikeID+".");
-        System.out.println("The usage in minutes is "+totalUsageInMinutes+".");
-        System.out.println("The total fare is "+calculateFare(usageInMinutes)+".");
-        System.out.println("============================\n");
-    }
+   private double calculateFare(int var1) {
+      this.totalFare = (double)1.0F + (double)0.5F * (double)var1;
+      return (double)1.0F + (double)0.5F * (double)var1;
+   }
 
-
-    public void printBikeDetails(){
-        System.out.println("======= Bike Details =======");
-        System.out.println("The bike's ID is " + bikeID+".");
-        System.out.println("The battery level is " + batteryLevel+"%.");
-        if(isAvailable){System.out.println("The bike is available.");}
-        else {System.out.println("The bike is not available.");}
-        System.out.println("The bike's distance travelled in " + kmDriven + " km.");
-        System.out.println("============================\n");
-    }
-
-    private double calculateFare(int usageInMinutes) {
-        totalFare = BASE_FARE + (PER_MINUTE_FARE*usageInMinutes);
-        return BASE_FARE + (PER_MINUTE_FARE * usageInMinutes);
-    }
-
-
-    public double getTotalFare() {
-        return totalFare;
-    }   
-   
-
-    
+   public double getTotalFare() {
+      return this.totalFare;
+   }
 }
-
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
-public  class Main {
-    public static void main(String[] args) {
-        ERyder bike1 = new ERyder(1001, 85, true, 125.6);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ERyder bike1 = new ERyder(1744,70,true,765.2f,"ngc232123","193848323");
         bike1.printBikeDetails();
-        bike1.printRideDetails(10);
-
-        ERyder bike2 = new ERyder(1002, 42, false, 289.3, "user_007", "9876543210");
+        bike1.printRideDetails(52);
+        ERyder bike2 = new ERyder(123,98,true,23.5);
         bike2.printBikeDetails();
         bike2.printRideDetails(30);
-        System.out.println("Correct usage of private method calculateFare(): Called internally via public method printBikeDetails.");
-        System.out.println("Total fare for bike1 (10 minutes ride):" + bike1.getTotalFare());
-        System.out.println("Total fare for bike2 (25 minutes ride):" + bike2.getTotalFare() + "USD");
 
- 
+
+        System.out.println("Can't use the private method directly. Must use another public method.");
+        System.out.println(bike2.getTotalFare());
         String sent1 = "I was very satisfied with the service.";
         String sent2 = "The e-Bike is quite comfortable to ride.";
         String sent3 = "The battery life of the e-Bike is impressive.";
         String sent4 = "The customer support was helpful and responsive.";
         String sent5 = "I would recommend this e-Bike to my friends and family.";
+        Feedback feedback = new Feedback("John", "Walker", "3422278532@qq.com");
+        feedback.analyseFeedBack(false, sent1, sent2, sent3, sent4, sent5); 
+        System.out.println(feedback);
 
-        Feedback feedBack = new Feedback("Alice", "Beauty", "4923das@163.com");
-        feedBack.analyseFeedBack(false, sent1, sent2, sent3, sent4, sent5); 
-        System.out.println(feedBack);
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.registration();
+        System.out.println(userRegistration);
 
-        UserRegistration userReg = new UserRegistration();
-        userReg.registration();
-        System.out.println("\n" + userReg);
+        AdminPanel adminPanel = new AdminPanel();
+        adminPanel.userManagementOptions();
     }
-    
 }
